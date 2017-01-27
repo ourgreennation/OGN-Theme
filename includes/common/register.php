68933adbbeb4fa30498447dffddd61177e477e36
Christian Chung <christian.m.chung@gmail.com>
2017-01-27 15:04:36 -0500

    ux(register): Remove ability to deselect pledges on profile edit screen

--- includes/common/register.php
+++ includes/common/register.php
@@ -26,6 +26,7 @@ final class Register {
 	public function register_hooks() {
 		remove_action( 'bp_before_account_details_fields', 'wsl_render_auth_widget_in_wp_login_form' );
 		add_action( 'bp_before_account_details_fields', array( $this, 'link_to_login' ) );
+		add_filter( 'bp_get_the_profile_field_options_checkbox', array( $this, 'disable_profile_pledges' ), 10, 5 );
 	}
 
 	/**
@@ -42,5 +43,42 @@ final class Register {
 			</a>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Disable Profile Pledges
+	 *
+	 * @param  string    $html     Field markup.
+	 * @param  \stdClass $obj      Option markup.
+	 * @param  int       $field_id Field ID.
+	 * @param  string    $selected Selected text.
+	 * @param  int       $key      Key in field loop.
+	 * @return string           HTML
+	 */
+	public function disable_profile_pledges( $html, $obj, $field_id, $selected, $key ) {
+		if ( ! bp_is_profile_edit() || ! property_exists( $obj, 'name' ) || 'option' !== $obj->type ) {
+			return $html;
+		}
+
+		// Conditions on which to act.
+		$conditions = array(
+			strpos( $obj->name, 'Member Pledge' ),
+			strpos( $obj->name, 'Privacy Policy' ),
+			isset( $field_id ),
+			isset( $key ),
+			);
+
+		// If conditions are met and we are agreeing, disable the checkbox if it's checked.
+		if ( false !== strpos( $obj->name, 'I agree' ) && ! empty( array_filter( $conditions ) ) ) {
+			$html = sprintf( '<label for="%3$s" class="option-label"><input %1$s type="checkbox" name="%2$s" id="%3$s" value="%4$s">%5$s</label>',
+				( ! $selected ) ? $selected : $selected . ' disabled="disabled"',
+				esc_attr( "field_{$this->field_obj->id}[]" ),
+				esc_attr( "field_{$obj->id}_{$key}" ),
+				esc_attr( stripslashes( $obj->name ) ),
+				esc_html( stripslashes( $obj->name ) )
+			);
+		}
+
+		return $html;
 	}
 }
