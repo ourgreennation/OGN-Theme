c14d93cacc4971dc915f41abf55d0ec2640ed036
Christian Chung <christian.m.chung@gmail.com>
2017-02-21 13:09:10 -0500

    ux(events): Suppress related posts on single events

--- includes/events/events.php
+++ includes/events/events.php
@@ -26,6 +26,8 @@ final class Events {
 	public function register_hooks() {
 		add_action( 'wp', array( $this, 'show_footer' ) );
 		add_action( 'wp', array( $this, 'fix_sidebar_placement' ) );
+
+		add_filter( 'jetpack_relatedposts_filter_options', array( $this, 'suppress_related_posts' ) );
 	}
 
 	/**
@@ -53,5 +55,18 @@ final class Events {
 			$classes[] = 'sidebar-left';
 			return $classes;
 		});
+	}
+
+	/**
+	 * Suppresses Jetpack Related Posts Module on Single Events
+	 * 
+	 * @param  array  $options An array of related posts options.
+	 * @return array           Filtered array of options.
+	 */
+	public function suppress_related_posts( $options ) {
+		if ( is_singular( 'tribe_events' ) ) {
+			$options['enabled'] = false;
+		}
+		return $options;
 	}
 }
